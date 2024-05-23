98134420fdb05e4ee0fda979de7a4bd92fce11eb
Jason Haak <83122452+jasonhaak@users.noreply.github.com>
2024-05-23 16:52:19 +0200

    Update analysis_avl_tree.php

--- src/analysis_avl_tree.php
+++ src/analysis_avl_tree.php
@@ -13,7 +13,6 @@ function analyse_avl_tree($size) {
 
    $time_start = microtime(true); // Set start time for measurement
    $avl_tree->insert_array($avl_array); // Execute function
-
    $time_end = microtime(true); // Set end time for measurement
    return $time_end - $time_start; // Calculate execution time
 }
